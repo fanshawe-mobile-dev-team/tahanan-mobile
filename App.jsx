97f66c6f46ea652b0c0feb6f19ef30d2506c6717
Hector Robles <hjt.robles@gmail.com>
2023-11-27 14:56:12 -0500

    include temporary screen selector

--- App.jsx
+++ App.jsx
@@ -1,6 +1,8 @@
-import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'react-native';
+import SelectDropdown from 'react-native-select-dropdown';
+import React, { useRef } from 'react';
 import DashboardScreen from './src/components/screens/Dashboard/DashboardScreen';
 import ProfileScreen from './src/components/screens/Profile/ProfileScreen';
 import LoginScreen from './src/components/screens/Login/LoginScreen';
@@ -14,31 +16,45 @@ import TaskListScreen from './src/components/screens/TaskList/TaskListScreen';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const navigationRef = useRef();
+  const screens = [
+    { name: 'Login', component: LoginScreen, options: { headerShown: false } },
+    { name: 'Register', component: RegisterScreen },
+    { name: 'PostRegister', component: PostRegisterScreen, options: { headerShown: false } },
+    { name: 'CreateHome', component: CreateHomeScreen },
+    { name: 'SearchHome', component: SearchHomeScreen },
+    { name: 'JoinHome', component: JoinHomeScreen },
+    { name: 'Dashboard', component: DashboardScreen, options: { headerShown: false } },
+    { name: 'TaskList', component: TaskListScreen },
+    { name: 'Profile', component: ProfileScreen },
+  ];
+
+  const handelSelectRoute = (screen) => {
+    navigationRef.current?.navigate(screen.name);
+  };
+
   return (
-    <NavigationContainer>
+
+    <NavigationContainer ref={navigationRef}>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen
-          name="PostRegister"
-          component={PostRegisterScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="CreateHome" component={CreateHomeScreen} />
-        <Stack.Screen name="SearchHome" component={SearchHomeScreen} />
-        <Stack.Screen name="JoinHome" component={JoinHomeScreen} />
-        <Stack.Screen
-          name="Dashboard"
-          component={DashboardScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="TaskList" component={TaskListScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
+        {screens.map((screen) => (
+          <Stack.Screen
+            key={`screen-${screen.name}`}
+            name={screen.name}
+            component={screen.component}
+            options={screen.options}
+          />
+        ))}
       </Stack.Navigator>
+      <StatusBar />
+      <SelectDropdown
+        data={screens}
+        buttonStyle={{ width: '100%', backgroundColor: 'black' }}
+        buttonTextStyle={{ color: 'white' }}
+        onSelect={handelSelectRoute}
+        buttonTextAfterSelection={(item) => item.name}
+        rowTextForSelection={(item) => item.name}
+      />
     </NavigationContainer>
   );
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tahanan-mobile
 Mobile App for Tahanan
+
+1. npm install
+2. install eslint globally
