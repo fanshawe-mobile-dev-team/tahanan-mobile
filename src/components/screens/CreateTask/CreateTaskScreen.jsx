a9160687b77f1830eb904a3273b19d1d9a1e5a22
levkulet <levielkulet@gmail.com>
2023-12-08 03:48:29 -0500

    Lev- added task list, placeholder avatar, fixed create task, dashboard, task list, commonstyles

--- src/components/screens/CreateTask/CreateTaskScreen.jsx
+++ src/components/screens/CreateTask/CreateTaskScreen.jsx
@@ -1,36 +1,20 @@
 import React, { useRef, useState, useEffect } from 'react';
 import {
-  View, Text, StyleSheet, ScrollView, KeyboardAvoidingView,
+  View, Text,
 } from 'react-native';
 import { Button, TextInput } from 'react-native-paper';
 import { DatePickerInput } from 'react-native-paper-dates';
-
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 16,
-    paddingVertical: 32,
-    flex: 1,
-  },
-  input: {
-    marginBottom: 20,
-  },
-  heading: {
-    fontWeight: 'bold',
-    marginBottom: 10,
-    fontSize: 24,
-  },
-  subheading: {
-    marginBottom: 28,
-  },
-});
+// import colors from '../../../theme/colors';
+import commonStyles from '../../../theme/commonStyles';
+import Container from '../../common/Container';
 
 function CreateTaskScreen({ navigation }) {
   // const [taskId, setTaskId] = useState();
   const [creatorId, setCreatorId] = useState('testUser');
   const [userIds, setUserIds] = useState([]);
   const [homeId, setHomeId] = useState('homeId');
-  const [name, setName] = useState('taskName');
-  const [description, setDescription] = useState('desc');
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
   const [createdAt, setCreatedAt] = useState(new Date());
   const [dueAt, setDueAt] = useState();
   const [isCompleted, setIsCompleted] = useState(false);
@@ -72,53 +56,53 @@ function CreateTaskScreen({ navigation }) {
   };
 
   return (
-    <KeyboardAvoidingView style={[styles.container]}>
-      <ScrollView>
-        <Text style={styles.heading}>Create a New Task</Text>
-        <Text style={styles.subheading}>
-          Enter the following information to create a task
-        </Text>
-        <View>
-          <TextInput
-            style={styles.input}
-            label="Task"
-            placeholder="Enter task name"
-            value={name}
-            onChangeText={setName}
-            onSubmitEditing={() => descriptionRef.current.focus()}
-          />
-          <TextInput
-            ref={descriptionRef}
-            style={styles.input}
-            label="Description"
-            placeholder="Enter your task description"
-            value={description}
-            onChangeText={setDescription}
-            onSubmitEditing={() => userIdRef.current.focus()}
-          />
-          <TextInput
-            ref={userIdRef}
-            style={styles.input}
-            label="Assigned Member/s"
-            placeholder="Enter username to assign"
-            value={userIds}
-            onChangeText={setUserIds}
-            onSubmitEditing={() => dueAtRef.current.focus()}
-          />
-          <DatePickerInput
-            locale="en"
-            label="Birthdate"
-            value={dueAt}
-            onChange={(d) => setDueAt(d)}
-            inputMode="start"
-          />
+    <Container>
+      <Text style={commonStyles.displayHeading}>Create a New Task</Text>
+      <Text style={commonStyles.displaySubheading}>
+        Enter the following information to create a task
+      </Text>
+      <View>
+        <TextInput
+          style={commonStyles.commonInput}
+          label="Task"
+          placeholder="Enter task name"
+          value={name}
+          onChangeText={setName}
+          onSubmitEditing={() => descriptionRef.current.focus()}
+        />
+        <TextInput
+          ref={descriptionRef}
+          style={commonStyles.commonInput}
+          label="Description"
+          placeholder="Enter your task description"
+          value={description}
+          onChangeText={setDescription}
+          onSubmitEditing={() => userIdRef.current.focus()}
+        />
+        <TextInput
+          ref={userIdRef}
+          style={commonStyles.commonInput}
+          label="Assigned Member/s"
+          placeholder="Enter username to assign"
+          value={userIds}
+          onChangeText={setUserIds}
+          onSubmitEditing={() => dueAtRef.current.focus()}
+        />
+        <DatePickerInput
+          locale="en"
+          label="Due Date"
+          value={dueAt}
+          onChange={(d) => setDueAt(d)}
+          inputMode="start"
+        />
 
+        <View style={commonStyles.buttonContainer}>
           <Button mode="contained" onPress={handleSubmit}>Add Task</Button>
 
-          <Button mode="contained" onPress={navigation.navigate('TaskList')}>Cancel</Button>
+          <Button mode="contained" style={commonStyles.greyButton} onPress={navigation.navigate('TaskList')}>Cancel</Button>
         </View>
-      </ScrollView>
-    </KeyboardAvoidingView>
+      </View>
+    </Container>
   );
 }
 
--- src/components/screens/TaskList/TaskListScreen.jsx
+++ src/components/screens/TaskList/TaskListScreen.jsx
@@ -1,8 +1,119 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useRef, useState } from 'react';
+import {
+  Text,
+  StyleSheet,
+  View,
+} from 'react-native';
+import {
+  List,
+  Surface,
+  Button,
+  IconButton,
+  MD3Colors,
+} from 'react-native-paper';
+// import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { DatePickerModal } from 'react-native-paper-dates';
+import commonStyles from '../../../theme/commonStyles';
+import Container from '../../common/Container';
+
+const BOTTOM_APPBAR_HEIGHT = 80;
+const MEDIUM_FAB_HEIGHT = 56;
 
 function TaskListScreen() {
-  return <Text>TaskListScreen</Text>;
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const formattedDate = currentDate.toDateString();
+  const [date, setDate] = React.useState(undefined);
+  const [open, setOpen] = React.useState(false);
+
+  const onDismissSingle = React.useCallback(() => {
+    setOpen(false);
+  }, [setOpen]);
+
+  const onConfirmSingle = React.useCallback(
+    (params) => {
+      setOpen(false);
+      setDate(params.date);
+      setCurrentDate(params.date);
+    },
+    [setOpen, setDate, setCurrentDate],
+  );
+
+  const openTaskHandler = async () => {
+    // TODO: create handler to open task
+  };
+
+  return (
+    <Container>
+      <Surface style={commonStyles.commonSurface} elevation={4}>
+        <View style={{ flexDirection: 'row' }}>
+          <Text style={commonStyles.dashbDate}>{formattedDate}</Text>
+          <View>
+            <IconButton
+              icon="calendar"
+              iconColor={MD3Colors.primary100}
+              size={20}
+              onPress={() => setOpen(true)}
+            />
+            <DatePickerModal
+              locale="en"
+              mode="single"
+              visible={open}
+              onDismiss={onDismissSingle}
+              date={date}
+              onConfirm={onConfirmSingle}
+            />
+          </View>
+        </View>
+      </Surface>
+      <List.Section style={{ marginTop: 24 }}>
+        <Text style={commonStyles.displayHeading}>Tasks</Text>
+
+        <List.Item
+          style={commonStyles.taskListItem}
+          title="Vacuum Living Room"
+          description="Use the vacuum cleaner to thoroughly clean the carpets, corners, and under furniture in the living room. Pay special attention to high-traffic areas."
+          left={(props) => <List.Icon {...props} icon="radiobox-marked" />}
+          right={(props) => <List.Icon {...props} icon="open-in-new" onPress={openTaskHandler} />}
+        />
+        <List.Item
+          style={commonStyles.taskListItem}
+          title="Clean Bathroom"
+          description="Scrub and sanitize all surfaces in the bathroom, including the toilet, sink, shower, and mirrors. Don't forget to replace used towels with fresh ones."
+          left={(props) => <List.Icon {...props} icon="radiobox-marked" />}
+          right={(props) => <List.Icon {...props} icon="open-in-new" onPress={openTaskHandler} />}
+        />
+        <List.Item
+          style={commonStyles.taskListItem}
+          title="Mop Kitchen Floor"
+          description="Mop the kitchen floor using a suitable cleaning solution. Pay attention to spills and stains. Move furniture if necessary to ensure thorough cleaning."
+          left={(props) => <List.Icon {...props} icon="radiobox-marked" />}
+          right={(props) => <List.Icon {...props} icon="open-in-new" onPress={openTaskHandler} />}
+        />
+        <List.Item
+          style={commonStyles.taskListItem}
+          title="Dust Furniture"
+          description="Dust all surfaces of furniture in the house using a microfiber cloth. Don't forget shelves, tabletops, and decorative items. Wipe surfaces if needed."
+          left={(props) => <List.Icon {...props} icon="radiobox-marked" />}
+          right={(props) => <List.Icon {...props} icon="open-in-new" onPress={openTaskHandler} />}
+        />
+        <List.Item
+          style={commonStyles.taskListItem}
+          title="Empty Trash Bins"
+          description="Empty all trash bins in the house. Replace liners if necessary. Take the trash outside to the designated collection area."
+          left={(props) => <List.Icon {...props} icon="radiobox-marked" />}
+          right={(props) => <List.Icon {...props} icon="open-in-new" onPress={openTaskHandler} />}
+        />
+        <List.Item
+          style={commonStyles.taskListItem}
+          title="Organize Closet"
+          description="Sort through clothing and items in the closet. Organize and declutter by donating or disposing of items not needed. Arrange clothes neatly and categorize items."
+          left={(props) => <List.Icon {...props} icon="radiobox-marked" />}
+          right={(props) => <List.Icon {...props} icon="open-in-new" onPress={openTaskHandler} />}
+        />
+      </List.Section>
+
+    </Container>
+  );
 }
 
 export default TaskListScreen;
