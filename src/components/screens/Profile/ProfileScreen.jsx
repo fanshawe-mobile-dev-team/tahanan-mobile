2817a7c262f06db1d6cf1e7c01ffb0edd4d359b7
cht45 <Keosocheat9@gmail.com>
2023-12-09 22:48:13 -0500

    create profile screen

--- src/components/screens/Profile/ProfileScreen.jsx
+++ src/components/screens/Profile/ProfileScreen.jsx
@@ -1,8 +1,79 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { Button, TextInput } from 'react-native-paper';
+import commonStyles from '../../../theme/commonStyles';
+import Container from '../../common/Container';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  imageContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  subHeading: {
+    fontWeight: 'bold',
+    marginVertical: 8,
+    textAlign: 'left',
+    fontSize: 20,
+  },
+});
 
 function ProfileScreen() {
-  return <Text>Profile Screen</Text>;
+  return (
+    <Container>
+      <View style={styles.imageContainer}>
+        <Image source={{ uri: 'https://picsum.photos/600' }} style={styles.image} />
+        <Text style={styles.subHeading}>
+          Leviel
+        </Text>
+        <Text style={styles.displayHeading}>
+          Home Admin
+        </Text>
+      </View>
+      <View>
+        <TextInput
+          style={commonStyles.commonInput}
+          label="First Name"
+          placeholder="Enter first name"
+        />
+        <TextInput
+          style={commonStyles.commonInput}
+          label="Last Name"
+          placeholder="Enter last name"
+        />
+        <TextInput
+          style={commonStyles.commonInput}
+          label="Email"
+          placeholder="Enter email"
+        />
+        <TextInput
+          style={commonStyles.commonInput}
+          label="Contact Number"
+          placeholder="Enter phone number"
+        />
+        <TextInput
+          style={commonStyles.commonInput}
+          label="Username"
+          placeholder="Enter username"
+        />
+        <Button mode="contained">Sign Out</Button>
+      </View>
+    </Container>
+  );
 }
 
 export default ProfileScreen;
